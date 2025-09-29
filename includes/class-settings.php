df92dc560d00e513d915301bf9f289574754aa41
yasuo3o3 <fujiwara@netservice.ne.jp>
2025-09-29 23:32:48 +0900

    WordPress標準サイズの品質入力をWordPress標準のメディア設定に統合

--- includes/class-settings.php
+++ includes/class-settings.php
@@ -9,6 +9,7 @@ class AndwImageControlSettings {
     public function __construct() {
         add_action('admin_init', array($this, 'register_settings'));
         add_action('admin_menu', array($this, 'add_settings_page'));
+        add_action('admin_init', array($this, 'modify_default_media_fields'));
     }
 
     public function register_settings() {
@@ -20,7 +21,7 @@ class AndwImageControlSettings {
 
         register_setting('media', 'andw_convert_png_to_jpeg', array(
             'type' => 'boolean',
-            'default' => false,
+            'default' => true,
         ));
 
         register_setting('media', 'andw_png_to_jpeg_quality', array(
@@ -74,11 +75,14 @@ class AndwImageControlSettings {
                     'default' => $custom_sizes[$size_name]['height'],
                 ));
             }
-            register_setting('media', 'andw_jpeg_quality_' . $size_name, array(
-                'type' => 'integer',
-                'sanitize_callback' => array($this, 'sanitize_quality'),
-                'default' => 82,
-            ));
+            // 標準サイズの場合は別途処理
+            if (!in_array($size_name, array('thumbnail', 'medium', 'large', 'full'))) {
+                register_setting('media', 'andw_jpeg_quality_' . $size_name, array(
+                    'type' => 'integer',
+                    'sanitize_callback' => array($this, 'sanitize_quality'),
+                    'default' => 82,
+                ));
+            }
         }
 
         add_settings_section(
@@ -101,14 +105,17 @@ class AndwImageControlSettings {
                     array('size_name' => $size_name, 'size_data' => $custom_sizes[$size_name])
                 );
             } else {
-                add_settings_field(
-                    'andw_jpeg_quality_' . $size_name,
-                    sprintf(__('%s JPEG品質', 'andw-image-control'), $size_label),
-                    array($this, 'quality_field_callback'),
-                    'media',
-                    'andw_image_control_section',
-                    array('option_name' => 'andw_jpeg_quality_' . $size_name, 'label' => '')
-                );
+                // 標準サイズの品質設定は標準セクションで処理するためここでは除外
+                if (!in_array($size_name, array('thumbnail', 'medium', 'large', 'full'))) {
+                    add_settings_field(
+                        'andw_jpeg_quality_' . $size_name,
+                        sprintf(__('%s JPEG品質', 'andw-image-control'), $size_label),
+                        array($this, 'quality_field_callback'),
+                        'media',
+                        'andw_image_control_section',
+                        array('option_name' => 'andw_jpeg_quality_' . $size_name, 'label' => '')
+                    );
+                }
             }
         }
 
@@ -262,6 +269,74 @@ class AndwImageControlSettings {
         $value = get_option($option_name, 85);
 
         echo '<input type="number" id="' . esc_attr($option_name) . '" name="' . esc_attr($option_name) . '" value="' . esc_attr($value) . '" min="1" max="100" class="small-text" />';
+    }
+
+    public function modify_default_media_fields() {
+        // WordPress標準の画像サイズ設定を再登録
+        register_setting('media', 'thumbnail_size_w', 'intval');
+        register_setting('media', 'thumbnail_size_h', 'intval');
+        register_setting('media', 'thumbnail_crop', 'intval');
+        register_setting('media', 'medium_size_w', 'intval');
+        register_setting('media', 'medium_size_h', 'intval');
+        register_setting('media', 'large_size_w', 'intval');
+        register_setting('media', 'large_size_h', 'intval');
+
+        // 標準サイズの品質設定を登録
+        register_setting('media', 'andw_jpeg_quality_thumbnail', array(
+            'type' => 'integer',
+            'sanitize_callback' => array($this, 'sanitize_quality'),
+            'default' => 82,
+        ));
+        register_setting('media', 'andw_jpeg_quality_medium', array(
+            'type' => 'integer',
+            'sanitize_callback' => array($this, 'sanitize_quality'),
+            'default' => 82,
+        ));
+        register_setting('media', 'andw_jpeg_quality_large', array(
+            'type' => 'integer',
+            'sanitize_callback' => array($this, 'sanitize_quality'),
+            'default' => 82,
+        ));
+
+        // WordPress標準の設定フィールドを置き換え
+        remove_settings_field('thumbnail_size_w', 'media', 'default');
+        remove_settings_field('thumbnail_size_h', 'media', 'default');
+        remove_settings_field('medium_size_w', 'media', 'default');
+        remove_settings_field('medium_size_h', 'media', 'default');
+        remove_settings_field('large_size_w', 'media', 'default');
+        remove_settings_field('large_size_h', 'media', 'default');
+
+        // カスタムフィールドを追加
+        add_settings_field('andw_thumbnail_size', __('サムネイル', 'andw-image-control'), array($this, 'standard_size_field_callback'), 'media', 'default', array('size_type' => 'thumbnail'));
+        add_settings_field('andw_medium_size', __('中サイズ', 'andw-image-control'), array($this, 'standard_size_field_callback'), 'media', 'default', array('size_type' => 'medium'));
+        add_settings_field('andw_large_size', __('大サイズ', 'andw-image-control'), array($this, 'standard_size_field_callback'), 'media', 'default', array('size_type' => 'large'));
+    }
+
+    public function standard_size_field_callback($args) {
+        $size_type = $args['size_type'];
+
+        $width_value = get_option($size_type . '_size_w', '');
+        $height_value = get_option($size_type . '_size_h', '');
+        $quality_value = get_option('andw_jpeg_quality_' . $size_type, 82);
+        $crop_value = ($size_type === 'thumbnail') ? get_option('thumbnail_crop', 0) : '';
+
+        echo '<div style="display: flex; align-items: center; gap: 8px; flex-wrap: wrap;">';
+        echo '<span>幅</span>';
+        echo '<input type="number" name="' . $size_type . '_size_w" value="' . esc_attr($width_value) . '" min="0" class="small-text" style="width: 70px;" />';
+        echo '<span>×</span>';
+        echo '<span>高さ</span>';
+        echo '<input type="number" name="' . $size_type . '_size_h" value="' . esc_attr($height_value) . '" min="0" class="small-text" style="width: 70px;" />';
+
+        if ($size_type === 'thumbnail') {
+            echo '<br><input type="checkbox" name="thumbnail_crop" value="1" ' . checked(1, $crop_value, false) . ' />';
+            echo '<label for="thumbnail_crop">' . __('切り抜いて正確なサイズに調整する', 'default') . '</label>';
+            echo '<br>';
+        }
+
+        echo '<span>／</span>';
+        echo '<span>品質</span>';
+        echo '<input type="number" name="andw_jpeg_quality_' . $size_type . '" value="' . esc_attr($quality_value) . '" min="1" max="100" class="small-text" style="width: 70px;" />';
+        echo '</div>';
     }
 
     public function select_field_callback($args) {
